387cb5674789f0e9b9d9d66643539c6151293555
mrmjpatra <mrmjpatra@gmail.com>
2023-01-04 02:29:14 +0530

    adding codes to assignment4 q7

--- ASSIGNMENT4/Assignment7.java
+++ ASSIGNMENT4/Assignment7.java
@@ -17,13 +17,20 @@ class MyThreadTest implements Runnable {
             System.out.println("Enter the number to find the factorial");
             int num = sc.nextInt();
             findFactorial(num);
-        }
-        // else if (compute.equals("gcd")) {
+        } else if (compute.equals("gcd")) {
+            System.out.println("Enter the two number for gcd");
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            
+            System.out.println("GCD of two number " + gcd(x, y));
+        } else if (compute.equals("lcm")) {
+            System.out.println("Enter the two number for lcm");
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int lcm=((x*y)/gcd(x, y));
+            System.out.println("LCM Of two number "+lcm);
 
-        // }else if (compute.equals("lcm")) {
-
-        // }
-        else if (compute.equals("sqrt")) {
+        } else if (compute.equals("sqrt")) {
             System.out.println("Enter the number to find square root");
             int num = sc.nextInt();
             findSquareRoot(num);
@@ -37,6 +44,17 @@ class MyThreadTest implements Runnable {
             x = x - y;
             System.out.println("After swapping two number : " + x + " & " + y);
         }
+
+    }
+
+    public int gcd(int x, int y) {
+        int gcd = 1;
+        for (int i = 1; i <= x && i <= y; i++) {
+            if (x % i == 0 && y % i == 0) {
+                gcd = i;
+            }
+        }
+        return gcd;
     }
 
     private void findSquareRoot(int num) {
@@ -58,16 +76,27 @@ class MyThreadTest implements Runnable {
 
 public class Assignment7 {
     public static void main(String[] args) throws Exception {
-        MyThreadTest test1=new MyThreadTest("factorial");
-        Thread t1=new Thread(test1);
+        MyThreadTest test1 = new MyThreadTest("factorial");
+        Thread t1 = new Thread(test1);
         t1.start();
         MyThreadTest test2 = new MyThreadTest("sqrt");
-        Thread t2=new Thread(test2);
+        Thread t2 = new Thread(test2);
         t1.join();
         t2.start();
         MyThreadTest test3 = new MyThreadTest("swap");
-        Thread t3=new Thread(test3);
+        Thread t3 = new Thread(test3);
         t2.join();
         t3.start();
+
+        MyThreadTest test4=new MyThreadTest("gcd");
+        Thread t4=new Thread(test4);
+        t3.join();
+        t4.start();
+
+        MyThreadTest test5=new MyThreadTest("lcm");
+        Thread t5=new Thread(test5);
+        t4.join();
+        t5.start();
+
     }
 }
