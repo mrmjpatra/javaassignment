dd993af42b76f69ac9a133d4491eea17b6910c5c
mrmjpatra <mrmjpatra@gmail.com>
2023-01-04 02:10:19 +0530

    Queue changes

--- ASSIGNMENT-3/classobject/Assignment10.java
+++ ASSIGNMENT-3/classobject/Assignment10.java
@@ -10,16 +10,26 @@ package classobject;
 import java.util.Scanner;
 
 class Queue {
-   private int arr[];
-   private int rear = -1, front = -1;
+    private int arr[];
+    private int rear = -1, front = -1;
 
     Queue() {
         arr = new int[10];
     }
 
+    public void read() {
+        Scanner sc = new Scanner(System.in);
+        // insert 10 elements into queue using insert method
+        System.out.println("Enter the elements to the queue");
+        for (int i = 0; i < 10; i++) {
+            int num = sc.nextInt();
+            insert(num);
+        }
+    }
+
     public void insert(int num) {
         if (isEmpty()) {
-            front=0;
+            front = 0;
         }
         if (!isFull()) {
             rear = rear + 1;
@@ -51,65 +61,66 @@ class Queue {
         return false;
     }
 
-    public void print(){
+    public void print() {
         System.out.println("Elements in Queue");
-        for (int i = front; i <=rear; i++) {
-            System.out.print(arr[i]+"\t");
+        for (int i = front; i <= rear; i++) {
+            System.out.print(arr[i] + "\t");
         }
         System.out.println();
     }
 
-    //replace even numbers available in queue with nearest prime numbers 
+    // replace even numbers available in queue with nearest prime numbers
 
-    public void replace(){
-        for (int i = front; i <=rear; i++) {
-            if (arr[i]%2==0) {
-                arr[i]=nearestPrimeNumber(arr[i]);
+    public void replace() {
+        for (int i = front; i <= rear; i++) {
+            if (arr[i] % 2 == 0) {
+                arr[i] = nearestPrimeNumber(arr[i]);
             }
         }
     }
 
     private int nearestPrimeNumber(int num) {
-        int val1=1,val2=1;
-        int count1=0,count2=0;
-        for (int i = num; i >=1; i--) {
+        int val1 = 1, val2 = 1;
+        int count1 = 0, count2 = 0;
+        for (int i = num; i >= 1; i--) {
             count1++;
-            if(isPrime(i)){
-                val1=i;
+            if (isPrime(i)) {
+                val1 = i;
                 break;
             }
         }
 
-        for (int i = num; i <=num*num; i++) {
+        for (int i = num; i <= num * num; i++) {
             count2++;
-            if(isPrime(i)){
-                val2=i;
+            if (isPrime(i)) {
+                val2 = i;
                 break;
             }
         }
 
-        if (count1>count2) {
+        if (count1 > count2) {
             return val2;
-        }else{
+        } else {
             return val1;
         }
     }
 
     private boolean isPrime(int i) {
-        if (i==1) {
+        if (i == 1) {
             return true;
         }
-        for (int j = 2; j <i; j++) {
-            if (i%j==0) {
+        for (int j = 2; j < i; j++) {
+            if (i % j == 0) {
                 return false;
             }
         }
         return true;
     }
-    public void displayOddPrimeNumber(){
-        int count1=0,count2=0;
-        for (int i = front; i <=rear; i++) {
-            if (arr[i]%2!=0) {
+
+    public void displayOddPrimeNumber() {
+        int count1 = 0, count2 = 0;
+        for (int i = front; i <= rear; i++) {
+            if (arr[i] % 2 != 0) {
                 count1++;
             }
             if (isPrime(arr[i])) {
@@ -117,31 +128,26 @@ class Queue {
             }
         }
 
-        System.out.println("Number of odd numbers :: "+count1);
-        System.out.println("Number of Prime numbers :: "+count2);
+        System.out.println("Number of odd numbers :: " + count1);
+        System.out.println("Number of Prime numbers :: " + count2);
     }
 }
 
 public class Assignment10 {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        Queue queue=new Queue();
-       //insert 10 elements into queue using insert method
-       System.out.println("Enter the elements to the queue");
-       for (int i = 0; i <10; i++) {
-            int num=sc.nextInt();
-            queue.insert(num);
-       }
-       //replace even numbers available in queue with nearest prime numbers
-       queue.replace();
-       //display number of odd and prime numbers.
-       queue.displayOddPrimeNumber();
-       //delete 3 elements from queue using delete method
-       queue.delete();
-       queue.delete();
-       queue.delete();
-       //display remaining element of queue.
-       queue.print();
-        
+        Scanner sc = new Scanner(System.in);
+        Queue queue = new Queue();
+        queue.read();
+        // replace even numbers available in queue with nearest prime numbers
+        //queue.replace();
+        // display number of odd and prime numbers.
+        queue.displayOddPrimeNumber();
+        // delete 3 elements from queue using delete method
+        queue.delete();
+        queue.delete();
+        queue.delete();
+        // display remaining element of queue.
+        queue.print();
+
     }
 }
